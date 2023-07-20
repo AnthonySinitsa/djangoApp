fdcfbd6b7ecd39c0f9bf96918514ee12cfe06f19
AnthonySinitsa <itonyemail@gmail.com>
2023-07-20 21:06:22 +0000

    command

--- djangoApp/models.py
+++ djangoApp/models.py
@@ -5,11 +5,10 @@ from django.urls import reverse
 class DjangoApp(models.Model):
   title = models.CharField(max_length=255)
   body = models.TextField()
-  author = models.ForeignKey(
-    get_user_model(),
-    on_delete=models.CASCADE,
-  )
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+  
   def __str__(self):
     return self.title
+  
   def get_absolute_url(self):
     return reverse('djangoApp_detail', args=[str(self.id)])
