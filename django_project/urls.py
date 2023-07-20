7f7670598aabebd6bc3911479fafa34a5b705aab
AnthonySinitsa <itonyemail@gmail.com>
2023-07-20 23:49:14 +0000

    command

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("pages/", include("pages.urls")),
     path("", include("djangoApp.urls")),
 ]
 
